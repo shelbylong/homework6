48f9038e283484a41715533c7fbbed669c3379c1
shelbylong <55207122+shelbylong@users.noreply.github.com>
2020-11-21 15:14:51 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -26,7 +26,7 @@ document.querySelector("#faster").addEventListener("click", function() {
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	video.currentTime = video.currentTime() + 5;
+	video.currentTime = video.currentTime + 5;
 	if (video.currentTime >= video.duration){
 		video.currentTime = 0;
 	}
