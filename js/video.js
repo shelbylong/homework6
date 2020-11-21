3c624a22a8198c82ce0b37282585c5b77382d9f5
shelbylong <55207122+shelbylong@users.noreply.github.com>
2020-11-21 10:39:38 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -6,9 +6,26 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+
+
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+
+
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+
+
 });
 
