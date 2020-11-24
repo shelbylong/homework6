a4dfbdee4d750e6cf8645d70ed3a1d645600acfa
shelbylong <55207122+shelbylong@users.noreply.github.com>
2020-11-23 20:20:38 -0500

    Add files via upload

---
+++ video.js
@@ -0,0 +1,39 @@
+var video = document.getElementById("myVideo");
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate*.9;
+	console.log(video.playbackRate);
+});
+	
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate*1.1;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime = video.currentTime() + 5;
+	if (video.currentTime >= video.duration){
+		video.currentTime = 0;
+	}
+	console.log(video.currentTime)
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	document.getElementById("mute").innerHTML = "Unmute";
+	video.muted = true;
+});
