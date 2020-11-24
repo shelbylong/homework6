26269b330d4069b1d6b1a59d0dde9c6b4a271054
shelbylong <55207122+shelbylong@users.noreply.github.com>
2020-11-23 23:17:04 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -49,6 +49,7 @@ document.querySelector("#mute").addEventListener("click", function() {
 
 document.querySelector("#volumeSlider").addEventListener("change", function() {
 	console.log(this.value);
+	video.volume = this.value/100;
 	let newVol = (this.value)*100 + "%";
 	document.getElementById("volume").innerHTML = newVol;
 });
